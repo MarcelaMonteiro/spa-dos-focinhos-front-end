2d69d18c4a73740bbfcf4fa2718b7b5cb4d8796c
Marcela <marcelapmont123@gmail.com>
2026-02-05 16:58:12 -0300

    fix font type for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Playfair_Display, Inter, Dancing_Script } from "next/font/google";
-import type { NextFontWithVariable } from "next/font";
+
 import Footer from "@/components/Footer";
 import { AuthProvider } from "@/src/context/AuthContext";
-
-export const playfair: NextFontWithVariable = Playfair_Display({
-	subsets: ["latin"],
-	weight: ["400", "600", "700"],
-	variable: "--font-playfair",
-});
-
-export const inter: NextFontWithVariable = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
-});
-
-export const dancing: NextFontWithVariable = Dancing_Script({
-	subsets: ["latin"],
-	variable: "--font-dancing",
-});
 
 export const metadata: Metadata = {
 	title: "Spa dos Focinhos",
@@ -33,9 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="pt-BR">
-			<body
-				className={`${inter.variable} ${playfair.variable} ${dancing.variable} `}
-			>
+			<body>
 				<AuthProvider>{children}</AuthProvider>
 			</body>
 		</html>
